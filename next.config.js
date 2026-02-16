f9f0c715683430b10155698c47ff747df5af2173
Ayman200509 <bhnayman@gmail.com>
2026-02-16 17:55:08 +0000

    Fix next.config.js warning

--- next.config.js
+++ next.config.js
@@ -15,9 +15,6 @@ const nextConfig = {
     },
     output: "standalone",
     serverExternalPackages: ['unzipper', '@aws-sdk/client-s3'],
-    experimental: {
-        serverComponentsExternalPackages: ['unzipper', '@aws-sdk/client-s3'],
-    },
 };
 
 module.exports = nextConfig;
